58ec91c74be11c51b64e5791863cd02c77b5e37c
Pablo Quezado <83846001+pabloquezado@users.noreply.github.com>
2025-04-07 20:38:33 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+function loadPage(){
+          window.location="pesquisa.html";
+        }
+const cardsPerPage = 2; // Número de cartas a aparecerem por página
+const dataContainer = document.getElementById('data-container'); 
+const pagination = document.getElementById('pagination'); 
+const prevButton = document.getElementById('prev'); 
+const nextButton = document.getElementById('next'); 
+const pageNumbers = document.getElementById('page-numbers'); 
+const pageLinks = document.querySelectorAll('.page-link'); 
+
+const cards = 
+	Array.from(dataContainer.getElementsByClassName('card')); 
+
+// Calcula o número total de páginas
+const totalPages = Math.ceil(cards.length / cardsPerPage); 
+let currentPage = 1; 
+
+// Função para mostrar cartas na página específica
+function displayPage(page) { 
+	const startIndex = (page - 1) * cardsPerPage; 
+	const endIndex = startIndex + cardsPerPage; 
+	cards.forEach((card, index) => { 
+		if (index >= startIndex && index < endIndex) { 
+			card.style.display = 'block'; 
+		} else { 
+			card.style.display = 'none'; 
+		} 
+	}); 
+} 
+
+// Função para atualizar botões da paginação e número de páginas
+function updatePagination() { 
+    pageNumbers.style="margin-left: 20px;"
+	pageNumbers.textContent = 
+		`Página ${currentPage} de ${totalPages}`; 
+	prevButton.disabled = currentPage === 1; 
+	nextButton.disabled = currentPage === totalPages; 
+	pageLinks.forEach((link) => { 
+		const page = parseInt(link.getAttribute('data-page')); 
+		link.classList.toggle('active', page === currentPage); 
+	}); 
+} 
+
+// Event listener para botão "anterior"
+prevButton.addEventListener('click', () => { 
+	if (currentPage > 1) { 
+		currentPage--; 
+		displayPage(currentPage); 
+		updatePagination(); 
+	} 
+}); 
+
+// Event listener para botão "próximo"
+nextButton.addEventListener('click', () => { 
+	if (currentPage < totalPages) { 
+		currentPage++; 
+		displayPage(currentPage); 
+		updatePagination(); 
+	} 
+}); 
+
+// Event listener para botões do número de páginas
+pageLinks.forEach((link) => { 
+	link.addEventListener('click', (e) => { 
+		e.preventDefault(); 
+		const page = parseInt(link.getAttribute('data-page')); 
+		if (page !== currentPage) { 
+			currentPage = page; 
+			displayPage(currentPage); 
+			updatePagination(); 
+		} 
+	}); 
+}); 
+
+// Carregamento inicial da página
+displayPage(currentPage); 
+updatePagination();
