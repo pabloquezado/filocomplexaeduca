81ad8f3e356d63157279cdae84832f07c4c711ff
Pablo Quezado <83846001+pabloquezado@users.noreply.github.com>
2025-04-23 01:05:57 -0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,18 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getDatabase, ref, set, get, push, child, update, onValue } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCCV3-Mjk8hYv5hba9qbCM0DUTXQNp9FDE",
     authDomain: "login-form-d0508.firebaseapp.com",
+    databaseURL: "https://login-form-d0508-default-rtdb.firebaseio.com/",
     projectId: "login-form-d0508",
     storageBucket: "login-form-d0508.firebasestorage.app",
     messagingSenderId: "319356886739",
     appId: "1:319356886739:web:c6f3d7a62f684e5079fe26"
   };
- 
-  // Inicializar Firebase
+
+  // Inicializa Firebase
   const app = initializeApp(firebaseConfig);
+
+  // Banco de dados
+  const database = getDatabase(app);
 
   const auth=getAuth();
   const db=getFirestore();
@@ -59,3 +64,33 @@ const firebaseConfig = {
         console.error('Erro no logout:', error);
     })
   })
+const button = document.getElementById("saveReal");
+button.addEventListener('click', () => {
+  var source = document.getElementById("here").value
+  var category = document.getElementById("options").value
+
+  if(source ==="" || category ==="1"){
+    alert('Campo vazio. Clique em "visualizar" para gerar o HTML e escolha uma categoria.')
+  }
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists() && source !=="" && category !=="1"){
+              
+                const db = getDatabase();
+                const userData=docSnap.data();
+                set(ref(db, 'users/' + userData.name + '/posts'), {
+                  página: source,
+                  categoria: category
+                });
+                alert(userData.name + ', sua página foi salva com sucesso!')
+                document.getElementById("here").value = '';
+                document.getElementById("options").value = "1";
+            }
+        })
+    }
+  })
+});
